f781f7be911ec2c0d78c13f97d83e09d8f01a961
Mehdi Jabarooti <mehdi73jabarooti@gmail.com>
2025-11-06 10:47:25 +0330

    Implement the complete and exit buttons

--- gui.py
+++ gui.py
@@ -7,23 +7,26 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=(45, 10))
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
+
+layout = [[label], [input_box, add_button], [list_box, edit_button, complete_button], [exit_button]]
 
 window = sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=layout,
                    font=("Helvetica", 12))
 
 while True:
     event, values = window.read()
-    print(1,event)
-    print(2, values)
-    print(3, values['todos'])
+    print(values)
     match event:
         case "Add":
             todos = functions.get_todos()
-            new_todo = values['todo']
-            todos.append(new_todo + "\n")
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
             functions.write_todos(todos)
-            window["todos"].update(todos)
+            window["todos"].update(values=todos)
+
         case "Edit":
             todo_to_edit = values['todos'][0]
             new_todo = values['todo']
@@ -33,8 +36,18 @@ while True:
             todos[index] = new_todo + "\n"
             functions.write_todos(todos)
             window["todos"].update(todos)
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update(value="")
+        case "Exit":
+            break
         case "todos":
             window["todo"].update(values['todos'][0])
         case sg.WINDOW_CLOSED:
             break
+
 window.close()
